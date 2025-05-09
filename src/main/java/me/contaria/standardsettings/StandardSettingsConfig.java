8c872912c668f70db5780bdb3051f03fc3236d24
contaria <kingcontaria@gmail.com>
2025-05-09 21:04:05 +0200

    fix: handle faulty language codes instead of trying to avoid them the LanguageManager may not be fully loaded by the time standardsettings wants to check if a language code is valid

--- src/main/java/me/contaria/standardsettings/StandardSettingsConfig.java
+++ src/main/java/me/contaria/standardsettings/StandardSettingsConfig.java
@@ -20,6 +20,8 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.options.*;
 import net.minecraft.client.render.entity.PlayerModelPart;
+import net.minecraft.client.resource.language.LanguageDefinition;
+import net.minecraft.client.resource.language.LanguageManager;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.Monitor;
 import net.minecraft.client.util.VideoMode;
@@ -162,7 +164,14 @@ public class StandardSettingsConfig implements SpeedrunConfig {
         this.register("showSubtitles", "options.sounds", Option.SUBTITLES);
 
         // Language
-        this.register("language", "options.language", StandardGameOptions::getLanguage, StandardGameOptions::setLanguage, option -> TextUtil.literal(MinecraftClient.getInstance().getLanguageManager().getLanguage(option.get()).toString()), option -> new ButtonWidget(0, 0, 120, 20, option.getText(), button -> MinecraftClient.getInstance().openScreen(new LanguageOptionsScreen(MinecraftClient.getInstance().currentScreen, options, MinecraftClient.getInstance().getLanguageManager()))));
+        this.register("language", "options.language", options -> options.language, (options, value) -> options.language = value, option -> {
+            LanguageManager manager = MinecraftClient.getInstance().getLanguageManager();
+            LanguageDefinition language = manager.getLanguage(option.get());
+            if (language == null) {
+                language = manager.getLanguage("en_us");
+            }
+            return TextUtil.literal(language.toString());
+        }, option -> new ButtonWidget(0, 0, 120, 20, option.getText(), button -> MinecraftClient.getInstance().openScreen(new LanguageOptionsScreen(MinecraftClient.getInstance().currentScreen, options, MinecraftClient.getInstance().getLanguageManager()))));
         this.register("forceUnicodeFont", "options.language", Option.FORCE_UNICODE_FONT);
 
         // Mouse Settings
